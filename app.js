892f9ac20a02b931029e9ab3fe30408b0ccc32b1
Vineet Mali <7038vineet@gmail.com>
2025-04-08 22:33:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const app = express();
+const app = express(); 
 const port = 8080; 
  
 
