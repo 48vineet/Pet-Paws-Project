3c7a8b32087a46d7be2ba278d035c1ae456da0f1
Vineet Mali <7038vineet@gmail.com>
2025-06-19 23:27:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const LocalStrategy =  require('passport-local');
 const session = require("express-session"); 
 const User = require('./models/user');
 const app = express();
-const port = 8080;
+const port = 8080; 
 
 // Middleware to parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
