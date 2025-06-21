2ccfb3cf14b392c550f67ef6723ab42d310f834f
Vineet Mali <7038vineet@gmail.com>
2025-06-21 21:50:58 +0530

    Update contactRouter.js

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const contactController = require("../controllers/contactController");
-
+ 
 
 router.route("/").get(contactController.conatct);
 
